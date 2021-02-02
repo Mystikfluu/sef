4ca1bc50095232f4d6108f1e2655d8b2ec77af6a
Mystikfluu <50167590+Mystikfluu@users.noreply.github.com>
2021-02-02 23:58:12 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,21 +1,39 @@
-if(getfenv().toggle == nil or getfenv().toggle == true) then getfenv().toggle = false wait(0.1) end
-getfenv().toggle = true
-if(getfenv().gui == true or getfenv().gui == nil) then
-  local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mystikfluu/uilib/master/uilib.lua"))()
-  local w = library:CreateWindow("Sword Effect Simulator")
-  local main = w:CreateFolder("Main")
-  main:Toggle("Instant Heal (nearly godmode)", function(value)
-    getfenv().godmode = value
-  end)
-  main:Toggle("Grind Power", function(value)
-    getfenv().grind = value
-  end)
-  main:Box("Power Thingy", "int", function(value)
-    getfenv().count = value or 8
-  end)
+repeat wait() until game
+repeat wait() until game.Players
+repeat wait() until game.Players.LocalPlayer
 
-  main:Box("Effect name", "string", function(value)
-    for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mystikfluu/uilib/master/uilib.lua"))()
+local w = library:CreateWindow("Sword Effect Simulator")
+local main = w:CreateFolder("Main")
+main:Toggle("Instant Heal (nearly godmode)", function(value)
+  _G.godmode = value
+end)
+main:Toggle("Grind Power", function(value)
+  _G.grind = value
+  _G.count = _G.count or 8
+  while wait() and _G.grind do
+    local lp = game:GetService("Players").LocalPlayer
+    local chars = lp.Character
+    local tool = chars:FindFirstChild("sword") or lp.Backpack:FindFirstChild("sword")
+    if(tool and tool:FindFirstChild("Handle")) then
+    if(tool.Handle:FindFirstChild("up")) then
+      if(tool.Handle.up:FindFirstChild("RemoteEvent")) then
+        local RE = tool.Handle.up.RemoteEvent
+        for a=1,_G.count do
+          RE:FireServer()
+        end
+      end
+    end
+    end
+  end
+
+end)
+main:Box("Power Thingy", "int", function(value)
+  _G.count = value or 8
+end)
+
+main:Box("Effect name", "string", function(value)
+  for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
     if(v:IsA("RemoteEvent")) then
       v:FireServer(value)
     end
@@ -30,66 +48,31 @@ end)
 main:Button("Kill ALL you can kill(not yet:tm:)", function()
 print("never :tm:")
 end)
-main:Button("Kill GUI", function()
-getfenv().toggle = false
-spawn(function()
-if(game.CoreGui:FindFirstChild("uiui") ~= nil) then game.CoreGui.uiui:Destroy() end
-if(game.CoreGui:FindFirstChild("uilibrary") ~= nil) then game.CoreGui.uilibrary:Destroy() end
-if(game.CoreGui:FindFirstChild("uiwindow") ~= nil) then game.CoreGui.uiwindow:Destroy() end
-end)
-end)
 
+
+if(_G.godmodecon) then
+  _G.godmodecon:Disconnect()
 end
-if(getfenv().godmodecon) then
-  getfenv().godmodecon:Disconnect()
-end
-getfenv().godmodecon = game:GetService("RunService").RenderStepped:Connect(function()
-  if(getfenv().godmode) then
+_G.godmodecon = game:GetService("RunService").RenderStepped:Connect(function()
+  if(_G.godmode) then
   game.ReplicatedStorage.Heal:FireServer()
   end
 end)
-if(getfenv().charcon) then
-  getfenv().charcon:Disconnect()
+if(_G.charcon) then
+  _G.charcon:Disconnect()
 end
-if(getfenv().humcon) then
-  getfenv().humcon:Disconnect()
+if(_G.humcon) then
+  _G.humcon:Disconnect()
 end
-getfenv().humcon = game.Players.LocalPlayer.Character.Humanoid.Changed:Connect(function()
-  if(getfenv().godmode) then
+_G.humcon = game.Players.LocalPlayer.Character.Humanoid.Changed:Connect(function()
+  if(_G.godmode) then
   game.ReplicatedStorage.Heal:FireServer()
   end
 end)
-getfenv().charcon = game.Players.LocalPlayer.CharacterAdded:Connect(function(char)
-getfenv().humcon = char.Humanoid.Changed:Connect(function()
-  if(getfenv().godmode) then
+_G.charcon = game.Players.LocalPlayer.CharacterAdded:Connect(function(char)
+_G.humcon = char.Humanoid.Changed:Connect(function()
+  if(_G.godmode) then
   game.ReplicatedStorage.Heal:FireServer()
   end
 end)
-end)
-
-getfenv().count = getfenv().count or 8
-coroutine.wrap(function()
-while wait() and getfenv().toggle do
-spawn(function()
-
-
-if(getfenv().grind) then
-local lp = game:GetService("Players").LocalPlayer
-local chars = lp.Character
-local tool = chars:FindFirstChild("sword") or lp.Backpack:FindFirstChild("sword")
-if(tool and tool:FindFirstChild("Handle")) then
-if(tool.Handle:FindFirstChild("up")) then
-  if(tool.Handle.up:FindFirstChild("RemoteEvent")) then
-    local RE = tool.Handle.up.RemoteEvent
-    local a = 0
-    repeat
-      RE:FireServer()
-      a = a + 1
-    until a >= getfenv().count
-  end
-end
-end
-end
-end)
-end
 end)
